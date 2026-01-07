e895beda5e0688d457aa53a93fb5bcb55d62a1c3
timbrown841 <timbrown841@outlook.com>
2026-01-07 11:50:51 +0000

    Update index.js

--- index.js
+++ index.js
@@ -12,10 +12,8 @@ const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
 // 2️⃣ middleware
 app.use(cors({
-  origin: [
-    'https://timbrown841.github.io',
-    'http://localhost:5500'
-    ]
+  origin: ['https://timbrown841.github.io']
+  credentials: false
 }));
 app.use(express.json()); 
 
@@ -407,4 +405,5 @@ const PORT = process.env.PORT || 10000;
 app.listen(PORT, () => {
   console.log(`🚀 Server running at http://localhost:${PORT}`);
 });
+
 
